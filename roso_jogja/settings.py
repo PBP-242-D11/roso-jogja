55a8a385792cebe3cb8d0b9de74f5bcca2364b99
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-10-09 12:02:51 +0700

    refactor: add prod and debug env

--- roso_jogja/settings.py
+++ roso_jogja/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&bk86)su+v@h*6)wb57_$+pd*7$53iu%*i8j0qr+ntb^wgz#ek"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = [
     "localhost",
