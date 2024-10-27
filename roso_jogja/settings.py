c192ac4ca40f1cd79adb094a1d7284d729e0c086
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-10-27 09:11:33 +0700

    chore: add django auto reload (run pip install again)

--- roso_jogja/settings.py
+++ roso_jogja/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_browser_reload",
     "main",
     "restaurant",
 ]
@@ -60,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "roso_jogja.urls"
