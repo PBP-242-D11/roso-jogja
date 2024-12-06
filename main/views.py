7dda008bd89e5b37cf408a46ce51d692a601040f
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-12-07 03:06:11 +0700

    feat: add mobile views for integration with flutter app

--- main/views.py
+++ main/views.py
@@ -82,5 +82,107 @@ def get_user_data(request):
     return JsonResponse(user_data)
 
 
+import json
+
+from django.contrib.auth import authenticate, get_user_model
+
 # API for Flutter App
 # =================================================================================================
+from django.views.decorators.csrf import csrf_exempt
+
+User = get_user_model()
+
+
+@csrf_exempt
+def mobile_login(request):
+    if request.method != "POST":
+        return JsonResponse(
+            {"status": False, "message": "Method not allowed"}, status=405
+        )
+
+    # Get username and password from request
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+
+    # Check if username and password are provided
+    if not username or not password:
+        return JsonResponse(
+            {"status": False, "message": "Username and password are required"},
+            status=400,
+        )
+
+    # Authenticate user
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Successful login
+            return JsonResponse(
+                {
+                    "username": user.username,
+                    "email": user.email,  # Add email if you want
+                    "status": True,
+                    "message": "Login sukses!",
+                    # You can add more user fields from your custom model here
+                },
+                status=200,
+            )
+        else:
+            return JsonResponse(
+                {"status": False, "message": "Login gagal, akun dinonaktifkan."},
+                status=401,
+            )
+    else:
+        return JsonResponse(
+            {
+                "status": False,
+                "message": "Login gagal, periksa kembali email atau kata sandi.",
+            },
+            status=401,
+        )
+
+
+@csrf_exempt
+def mobile_register(request):
+    if request.method != "POST":
+        return JsonResponse(
+            {"status": False, "message": "Method not allowed"}, status=405
+        )
+
+    data = json.loads(request.body)
+    username = data.get("username")
+    password1 = data.get("password1")
+    password2 = data.get("password2")
+
+    if not username or not password1 or not password2:
+        return JsonResponse(
+            {"status": False, "message": "Username and password are required"},
+            status=400,
+        )
+
+    if password1 != password2:
+        return JsonResponse(
+            {"status": False, "message": "Password tidak sama"},
+            status=400,
+        )
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse(
+            {"status": False, "message": "Username sudah digunakan"},
+            status=400,
+        )
+
+    user = User.objects.create_user(username=username, password=password1)
+    user.save()
+
+    return JsonResponse(
+        {
+            "username": user.username,
+            "email": user.email,  # Add email if you want
+            "status": "success",
+            "message": "Register sukses!",
+            # You can add more user fields from your custom model here
+        },
+        status=200,
+    )
